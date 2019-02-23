75eb9b483f8340f4fc5d7627a9f074581a39f322
karljacques <karljacques95@gmail.com>
2019-02-23 20:08:42 +0000

    Automatically calculate fuel required

--- client/src/components/game/docked/station/Marketplace.vue
+++ client/src/components/game/docked/station/Marketplace.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
         <h2>Buy</h2>
-        <v-data-table :headers="headers" :items="items">
+        <v-data-table :headers="headers" :items="fromServer">
             <template slot="items" slot-scope="props">
                 <td>{{ props.item.name }}</td>
-                <td>{{ props.item.quantity }}</td>
+                <td>{{ props.item.stock }}</td>
                 <td>{{ props.item.price|currency }}</td>
                 <td>
-                    <v-text-field type="number" placeholder="Quantity"></v-text-field>
+                    <v-text-field v-model="quantities[props.item.id]" type="number" placeholder="Quantity"></v-text-field>
                 </td>
+                <td>{{ (quantities[props.item.id] ? Math.min(props.item.stock, quantities[props.item.id]) : 0) * props.item.price|currency }}</td>
                 <td>
                     <v-btn color="primary" outline>Buy</v-btn>
                 </td>
@@ -18,10 +19,27 @@
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+
     export default {
-        name: "Marketplace",
+        name:     "Marketplace",
+        computed: {
+            ...mapGetters('vessel', ['ship']),
+            fuelRequired () {
+                const diff = this.ship.max_fuel - this.ship.fuel;
+                if (diff < 0) {
+                    return 0;
+                }
+
+                return diff;
+            }
+        },
+        created () {
+            this.$set(this.quantities, 1, this.fuelRequired);
+        },
         data () {
             return {
+                test: 'hello',
                 headers: [
                     {
                         text:  'Commodity',
@@ -29,36 +47,48 @@
                     },
                     {
                         text:  'In Stock',
-                        value: 'quantity'
+                        value: 'stock'
                     },
                     {
                         text:  'Price',
                         value: 'price'
                     }, {
+                        text: 'Quantity',
+                        value:    null,
+                        sortable: false
+                    },
+                    {
+                        text: 'Total',
                         value: null,
                         sortable: false
-                    }, {
-                        value: null,
+                    },
+                    {
+                        value:    null,
                         sortable: false
                     }
                 ],
-                items:   [
+                fromServer: [
                     {
-                        name:     'Fuel',
-                        price:    2500,
-                        quantity: 532
+                        id: 1,
+                        name:  'Fuel',
+                        price: 2500,
+                        stock: 2032
                     },
                     {
-                        name:     'Metals',
-                        price:    3200,
-                        quantity: 200
+                        id: 2,
+                        name:  'Metals',
+                        price: 3200,
+                        stock: 200
                     },
                     {
-                        name:     'Chemicals',
-                        price:    7800,
-                        quantity: 200
+                        id: 3,
+                        name:  'Chemicals',
+                        price: 7800,
+                        stock: 200
                     }
-                ]
+                ],
+                quantities: {
+                }
             }
         }
     }
